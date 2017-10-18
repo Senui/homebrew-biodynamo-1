47358c1b8d13344fe804791785495dba803b23e0
Ahmad Hesam <hesam212@gmail.com>
2017-10-18 16:31:22 +0200

    Update hashsum

--- biodynamo.rb
+++ biodynamo.rb
@@ -3,7 +3,7 @@ class Biodynamo < Formula
   homepage "https://github.com/Senui/biodynamo"
   #TODO: replace with correct link / hashsum
   url "https://github.com/Senui/biodynamo/releases/download/v0.0.9/biodynamo-0.1.0.sierra.tar.gz"
-  sha256 "87c15599f594e25e093d8b58dabf7396805311aebdd37342fbab79be5a38237f"
+  sha256 "6a45b761be081407949d7445c8df31387694cd4901dfa87fb4fb314d0b56e5e1"
   version "0.1.0"
 
   depends_on "Senui/biodynamo/bdm_root" => :build
