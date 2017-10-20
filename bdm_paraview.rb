b98954385dc285287b8afbf50c1006df2b24c9c6
Ahmad Hesam <hesam212@gmail.com>
2017-10-20 15:22:55 +0200

    Rebuild ParaView libraries with RPATH install_name

--- bdm_paraview.rb
+++ bdm_paraview.rb
@@ -1,8 +1,8 @@
 class BdmParaview < Formula
   desc "An open-source, multi-platform data analysis and visualization application"
   homepage "https://gitlab.kitware.com/paraview/paraview"
-  url "https://cernbox.cern.ch/index.php/s/BbFptgxo2K565IS/download?path=%2F&files=paraview-5.4_macos64_clang81.tar.gz"
-  sha256 "855d33b83a4e5a5de93bbb3fea5cb0cf552110b8491ec5c368ee8cac23900f11"
+  url "https://cernbox.cern.ch/index.php/s/BbFptgxo2K565IS/download?path=%2F&files=paraview-5.4_macos64_llvm-5.0.tar.gz"
+  sha256 "62791ec4453f66d2ea2730f4ae98fc8bede6d7e065b84020546f14bcf7c96df9"
   version "5.4.1"
   
   bottle :unneeded
