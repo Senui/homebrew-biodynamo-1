9a4df8c0e26d98bbfa8220d20aaa4bd1ab914bc3
Ahmad Hesam <hesam212@gmail.com>
2017-10-27 14:30:23 +0200

    More rpath patches

--- bdm_paraview.rb
+++ bdm_paraview.rb
@@ -2,7 +2,7 @@ class BdmParaview < Formula
   desc "An open-source, multi-platform data analysis and visualization application"
   homepage "https://gitlab.kitware.com/paraview/paraview"
   url "https://cernbox.cern.ch/index.php/s/BbFptgxo2K565IS/download?path=%2F&files=paraview-5.4_macos64_llvm-5.0.tar.gz"
-  sha256 "412c664ba0a23b0d54ebee6e0afd4905146ef2cf30a9eeac157f0b8bd1dcc913"
+  sha256 "67ffe4891cc03ba7a31caf5d83dcec4d6ece67c7bf18250be72ae61810de85f9"
   version "5.4.1"
   
   bottle :unneeded
