fd48c806c072c0da3bb2d8395f5f52a0380b0f95
Ahmad Hesam <hesam212@gmail.com>
2017-10-27 02:41:44 +0200

    Patch remaining libraries with rpath
    All the .so files were still pointing relative to the executable. They are now pointing to rpath

--- bdm_paraview.rb
+++ bdm_paraview.rb
@@ -2,7 +2,7 @@ class BdmParaview < Formula
   desc "An open-source, multi-platform data analysis and visualization application"
   homepage "https://gitlab.kitware.com/paraview/paraview"
   url "https://cernbox.cern.ch/index.php/s/BbFptgxo2K565IS/download?path=%2F&files=paraview-5.4_macos64_llvm-5.0.tar.gz"
-  sha256 "9b96c762854e41099744a822e4b09a84242ecf0523a019ed95dca76e21595c7e"
+  sha256 "412c664ba0a23b0d54ebee6e0afd4905146ef2cf30a9eeac157f0b8bd1dcc913"
   version "5.4.1"
   
   bottle :unneeded
