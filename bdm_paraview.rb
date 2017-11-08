25f2bf3a9a69d6b8c468afd4f9b8dfa75e75bb64
Ahmad Hesam <hesam212@gmail.com>
2017-11-08 19:58:44 +0100

    Patch the main binary's rpath list as well
    Forgot to patch the paraview.app/Contents/MacOS/paraview rpath list with the development directory

--- bdm_paraview.rb
+++ bdm_paraview.rb
@@ -2,7 +2,7 @@ class BdmParaview < Formula
   desc "An open-source, multi-platform data analysis and visualization application"
   homepage "https://gitlab.kitware.com/paraview/paraview"
   url "https://cernbox.cern.ch/index.php/s/BbFptgxo2K565IS/download?path=%2F&files=paraview-5.4_macos64_llvm-5.0.tar.gz"
-  sha256 "78c2a34863152ebf7faa0e479c7299887e9cc17126b64780c59a6679fe37ad9f"
+  sha256 "e888aca96d30f045b07b6a433265ccc6ae7db241e416ee97ed6ceecd9340ec26"
   version "5.4.1"
   
   bottle :unneeded
