fd3ebd6c441e4931a4774927335e6347dd14998f
Ahmad Hesam <hesam212@gmail.com>
2017-11-08 19:34:19 +0100

    Patch ParaView binaries with development rpath
    Add the development directory to the list of rpaths that the ParaView binaries should look in

--- bdm_paraview.rb
+++ bdm_paraview.rb
@@ -2,7 +2,7 @@ class BdmParaview < Formula
   desc "An open-source, multi-platform data analysis and visualization application"
   homepage "https://gitlab.kitware.com/paraview/paraview"
   url "https://cernbox.cern.ch/index.php/s/BbFptgxo2K565IS/download?path=%2F&files=paraview-5.4_macos64_llvm-5.0.tar.gz"
-  sha256 "67ffe4891cc03ba7a31caf5d83dcec4d6ece67c7bf18250be72ae61810de85f9"
+  sha256 "78c2a34863152ebf7faa0e479c7299887e9cc17126b64780c59a6679fe37ad9f"
   version "5.4.1"
   
   bottle :unneeded
