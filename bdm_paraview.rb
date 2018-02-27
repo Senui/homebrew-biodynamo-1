b7ef25d9d7f5c15dcd0544c6fd50a0bddc52afb1
Ahmad Hesam <hesam212@gmail.com>
2018-02-27 16:33:33 +0100

    Update shasum
    Added custom plugin to our ParaView distribution

--- bdm_paraview.rb
+++ bdm_paraview.rb
@@ -2,7 +2,7 @@ class BdmParaview < Formula
   desc "An open-source, multi-platform data analysis and visualization application"
   homepage "https://gitlab.kitware.com/paraview/paraview"
   url "https://cernbox.cern.ch/index.php/s/BbFptgxo2K565IS/download?path=%2F&files=paraview-5.4_macos64_llvm-5.0.tar.gz"
-  sha256 "e888aca96d30f045b07b6a433265ccc6ae7db241e416ee97ed6ceecd9340ec26"
+  sha256 "1f17550e5eb685abd5b71edc61ab925920bcd98dd9550b0720e81eb474e924fd"
   version "5.4.1"
   
   bottle :unneeded
